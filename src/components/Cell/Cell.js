2de27f1039b7965e1ba000aad7f7051da5168951
Alex Koval <al3xander.koval@gmail.com>
2018-04-24 23:37:34 +0300

    fourth commit

--- src/components/Cell/Cell.js
+++ src/components/Cell/Cell.js
@@ -2,20 +2,62 @@ import React from 'react'
 import './Cell.css'
 
 class Cell extends React.Component {
-	constructor(props) {
-		super(props);
-		this.showValue = this.showValue.bind(this);
-	}
+  constructor(props) {
+    super(props);
+    this.processClick = this.processClick.bind(this);
+    this.state = {
+      isVisible: false,
+      isActive: true,
+      isMatched: false
+    }
+  }
 
-	showValue(event) {
-		console.log(event.target.innerText)
-		this.props.handler(event.target.innerText);
-		event.target.style.color = 'black';
-	}
+  processClick(event) {
+    this.showValue();
 
-	render() {
-		return <div className="cell" onClick={this.showValue}>{this.props.value}</div>;
-	}
+    const prevValue = this.props.prevValue;
+    const currentValue = event.target.innerHTML;
+
+    if (prevValue != null) {
+      if (prevValue.props.value == currentValue) {
+        this.disableCell();
+        this.markGreen();
+        prevValue.disableCell();
+        prevValue.markGreen();
+        this.props.handler(null);
+      } else {
+        setTimeout(() => {
+          this.hideValue();
+          prevValue.hideValue();
+          this.props.handler(null);
+        }, 500);
+      }
+    } else {
+      this.props.handler(this);
+    }
+  }
+
+  render() {
+    return <div
+      className={'cell ' + (this.state.isVisible ? 'visible' : 'hidden') + ' ' + (this.state.isMatched ? 'is-green' : '')}
+      onClick={this.state.isActive ? this.processClick : null}>{this.props.value}</div>;
+  }
+
+  showValue() {
+    this.setState({isVisible: true});
+  }
+
+  hideValue() {
+    this.setState({isVisible: false});
+  }
+
+  disableCell() {
+    this.setState({isActive: false})
+  }
+
+  markGreen() {
+    this.setState({isMatched: true})
+  }
 }
 
 export default Cell
--- src/components/Field/Field.js
+++ src/components/Field/Field.js
@@ -3,48 +3,46 @@ import Cell from '../Cell/Cell'
 import './Field.css';
 
 class Field extends React.Component {
- constructor(props) {
-  super(props);
-  this.state = { 
-  	nums: [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8],
-  	prevValue: null 
+  constructor(props) {
+    super(props);
+    this.state = {
+      nums: [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8],
+      prevValue: null
+    };
+    this.shuffle = this.shuffle.bind(this);
+    this.setPrevValue = this.setPrevValue.bind(this);
   }
-  this.shuffle = this.shuffle.bind(this);
-  this.setPrevValue = this.setPrevValue.bind(this);
- }
 
-setPrevValue(value) {
-	console.log(value)
-	this.setState({
-		prevValue: value
-	});
-}
+  setPrevValue(value) {
+    this.setState({
+      prevValue: value
+    });
+  }
 
- shuffle() {
-   this.setState((prevState) => {
-   	return {
-   		nums: prevState.nums.sort( () => { return Math.random() - 0.5})
-   	}
-   });
- }
+  shuffle() {
+    this.setState((prevState) => {
+      return {
+        nums: prevState.nums.sort(() => Math.random() - 0.5)
+      }
+    });
+  }
 
- componentWillMount() {
- 	this.shuffle();
- }
-
+  componentWillMount() {
+    this.shuffle();
+  }
 
   render() {
-   const items =  this.state.nums.map((value) => <Cell value={value} handler={this.setPrevValue} />)
-    return(
-   <div className="game-wrapper">	
-   	<button onClick={this.shuffle}>Shuffle</button>		
-    <div id="game">
-     {items}
-    </div>
-    {this.state.prevValue}
-   </div>
-
-  );
+    const items = this.state.nums.map((value) =>
+      <Cell value={value} handler={this.setPrevValue} prevValue={this.state.prevValue}/>
+    );
+    return (
+      <div className="game-wrapper">
+        <button onClick={this.shuffle}>Shuffle</button>
+        <div id="game">
+          {items}
+        </div>
+      </div>
+    );
   }
 }
 
